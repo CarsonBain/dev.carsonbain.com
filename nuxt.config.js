96ddd55ad9099a53db6d2f954a82a230774120ab
Carson Bain <carsonbain@Carsons-MacBook-Air.local>
2021-11-18 22:06:52 -0500

    feat: change code theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,7 +102,7 @@ export default {
   content: {
     markdown: {
       prism: {
-        theme: 'prism-themes/themes/prism-nord.css',
+        theme: 'prism-themes/themes/prism-material-oceanic.css',
       },
     },
   },
